0e81f8cfcdf0fe0445796feefa1b08541f59482d
Cytan <2246203158@qq.com>
2022-09-02 16:08:57 +0800

    cytan update

--- config/MysqlConfig.py
+++ config/MysqlConfig.py
@@ -1,11 +1,3 @@
-class NLPCCConfig(object):
-    def __init__(self) -> None:
-        self.host = '192.168.126.122'
-        self.port = 6789
-        self.user = 'yhjia'
-        self.password = 'yhjia'
-        self.database = 'nlpcc'
-        self.charset = 'utf8'
 
 class CCKSConfig(object):
     def __init__(self) -> None:
