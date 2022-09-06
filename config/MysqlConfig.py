9cd96d0a4ec9f60977c6983ceb13097a37b028ad
Cytan <2246203158@qq.com>
2022-09-06 16:20:06 +0800

    cytan update MysqlComfig

--- config/MysqlConfig.py
+++ config/MysqlConfig.py
@@ -1,11 +1,11 @@
 
 class CCKSConfig(object):
     def __init__(self) -> None:
-        self.host = '192.168.126.139'
-        self.port = 3306
-        self.user = 'yhchen'
-        self.password = 'yhchen'
-        self.database = 'ccks2021'
-        self.charset = 'utf8mb4'
+        self.host = 'XX'
+        self.port = XX
+        self.user = 'XX'
+        self.password = 'XX'
+        self.database = 'XX'
+        self.charset = 'XXX'
 
 
